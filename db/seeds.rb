3cb81323954e1c47220b08ca0dd9b662779ed920
Marek Dziewit <marek.dziewit@mobica.com>
2016-07-30 21:45:14 -0700

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(email: "marek2901@gmail.com", password: "knownpassword")
+
+teachers = []
+3.times do |n|
+  teachers << Teacher.create(first_name: "Teacher#{n}", last_name: "Example", title: "Dr.")
+end
+
+subjects = []
+5.times do |n|
+  subjects << Subject.create(title: "Subject#{n}", teacher: teachers.sample)
+end
+
+students = []
+25.times do |n|
+  students << Student.create(first_name: "Student#{n}", last_name: "Exampleee")
+end
+
+4.times do
+  students.each do |student|
+    participant = Participant.new
+    participant.participant_id = student.id
+    participant.subject_id = subjects.sample.id
+    participant.save
+  end
+end
